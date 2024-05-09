b473f8453d38f2a5d18778d0b70adc52c35c5071
Angel Fuentes <angel09reyes29@gmail.com>
2024-05-08 23:18:53 -0600

    teams and player

---
+++ src/const/teams.ts
@@ -0,0 +1,14 @@
+const teams = [
+    {
+        id: 1,
+        name: 'Manchester City',
+        logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/eb/Manchester_City_FC_badge.svg/1200px-Manchester_City_FC_badge.svg.png'
+    },
+    {
+        name: 'Team 2',
+        id: 2,
+        logo: 'https://cdnb.20m.es/sites/30/2021/01/1010px-Manchester_United_FC_crest.svg.png'
+    },
+]
+
+export default teams;
