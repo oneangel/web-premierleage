e35c73133e968b5c519257adbacd763ade800f9f
Angel Fuentes <angel09reyes29@gmail.com>
2024-05-15 19:35:38 -0600

    implement daisy ui

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 
 const Table = () => {
-  return <div></div>;
+  return (
+    <div className="overflow-x-auto">
+      <table className="table">
+        <tbody>
+          {/* row 1 */}
+          <tr>
+            <td>
+              <div className="flex items-center gap-3">
+                <div className="avatar">
+                  <div className="mask mask-squircle w-12 h-12">
+                    <img
+                      src="https://img.daisyui.com/tailwind-css-component-profile-2@56w.png"
+                      alt="Avatar Tailwind CSS Component"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div className="font-bold">Hart Hagerty</div>
+                  <div className="text-sm opacity-50">United States</div>
+                </div>
+              </div>
+            </td>
+            <td>Purple</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Table;
